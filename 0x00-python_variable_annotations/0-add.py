891500cf34485b7411dc82cf624e7ef4529ff640
hendrixxD <maticsjnr@gmail.com>
2023-03-06 13:13:22 +0100

    0-add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""basic annotations - add"""
+
+# from typing import Float
+
+def add(a: float, b: float):
+    """
+    a: float
+    b: float
+    return sum(float)
+    """
+    return a + b
---
+++ 0x00-python_variable_annotations/type.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+from typing import List
+
+# raises an error.
+x: str = 'name'
+
+names: List[List[str]] = [['3'], ['s']]
+print(names)
