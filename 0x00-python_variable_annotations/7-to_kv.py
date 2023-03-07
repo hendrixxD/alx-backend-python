d192b53468f75912d638aae95c711f6abaa4c471
hendrixxD <maticsjnr@gmail.com>
2023-03-07 21:16:15 +0100

    complex types

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,8 +4,8 @@
 from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     args: k, v
     """
-    return (k, v**2)
+    return k, (v)**2
