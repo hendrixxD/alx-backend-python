cd814522e2dfc251d6489949b28796a72a145878
hendrixxD <maticsjnr@gmail.com>
2023-03-08 01:22:17 +0100

    more invloved type annotation

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -7,9 +7,11 @@ from typing import Mapping, Any, Union, TypeVar, Optional
 
 T = TypeVar("T")
 
+
 def safely_get_value(dct: Mapping,
                      key: Any,
                      default: Optional[T] = None) -> Union[Any, T]:
+    """comlex annotations """
     if key in dct:
         return dct[key]
     else:
